edcd30c77f51913e4dfb746b358ed0e073b7161f
Thomas Zastrow <thomas.zastrow@mpcdf.mpg.de>
2017-02-14 15:28:51 +0100

    Deleting echos

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $request = str_replace("/collections/api.php/", "", $request);
 $request = rtrim($request, "/");
 
 $rec = split("/", $request);
-
+#echo "REC: " . sizeof($rec) . "\n";
 $itemName = "";
 
 if(sizeof($rec) == 2){
@@ -19,6 +19,7 @@ $itemName = $rec[2];
 $rec[1] = "./store/" . $rec[1];
 }
 
+#echo "REQUEST: $request\n";
 
 ###### Implementing the functions:
 
